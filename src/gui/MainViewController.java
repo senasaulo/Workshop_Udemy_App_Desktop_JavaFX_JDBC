f9ff6bccd6088e175f1cf541156aee99f840a111
senasaulo <srs3@aluno.ifal.edu.br>
2026-04-16 15:15:10 -0300

    Criação do DepartamentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartamentList.fxml");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
